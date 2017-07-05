22855a756aa469ffb97211c6fe94d17d3dacdf9a
Michael Cittar <mcittar@gmail.com>
2017-07-05 12:28:48 -0700

    Create Publication Actions

---
+++ frontend/actions/publication_actions.js
@@ -0,0 +1,13 @@
+import * as APIUtil from '../util/publication_api_util';
+export const RECEIVE_PUBLICATIONS = 'RECEIVE_PUBLICATIONS';
+
+export const scrapePublications = () => dispatch => (
+  APIUtil.scrapePublications().then(
+    publications => dispatch(receivePublications)
+  )
+);
+
+export const receivePublications = publications => ({
+  type: RECEIVE_PUBLICATIONS,
+  publications
+});
