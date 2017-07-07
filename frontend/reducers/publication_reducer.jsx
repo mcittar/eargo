316bfecee859e96bf5939e94b209e571f3dee265
Michael Cittar <mcittar@gmail.com>
2017-07-07 09:11:51 -0700

    Clean up extra boilerplate code

--- frontend/reducers/publication_reducer.jsx
+++ frontend/reducers/publication_reducer.jsx
@@ -1,6 +1,7 @@
 import { RECEIVE_PUBLICATIONS } from '../actions/publication_actions';
 
 const PublicationReducer = (state = [], action) => {
+  Object.freeze(state);
   switch(action.type){
     case RECEIVE_PUBLICATIONS:
       return action.publications;
--- frontend/util/project_api_util.js
+++
@@ -1,6 +0,0 @@
-export const scrapeGif = () => {
-  return $.ajax({
-    method: 'GET',
-    url: 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC'
-  });
-};
